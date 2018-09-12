141a35f082e3b470c76758da566f59543f6c3472
Kanta Demizu <demizu.1203@gmail.com>
2018-09-12 16:27:43 +0900

    [CHANGED] Use Prezto

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,8 +2,9 @@
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zprofile ~/.zprofile
+#ln -sf ~/dotfiles/.zshrc ~/.zshrc
+#ln -sf ~/dotfiles/.zprofile ~/.zprofile
+
 ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
 ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
 
