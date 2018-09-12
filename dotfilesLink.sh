675df6bc7c6b0573a96fa51cd3b19c62690f61d4
Kanta Demizu <demizu.1203@gmail.com>
2018-09-12 12:27:21 +0900

    [ADD] makeSymlinks script

---
+++ dotfilesLink.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
+ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
+
