723692f49d7ad3f9fc8ae253545832d7e8410860
Sw_Saturn <demizu.1203@gmail.com>
2019-01-20 13:16:35 +0900

    [ADD] LinkScript

--- .zprofile
+++
@@ -1,7 +0,0 @@
-if [ $(uname) = Darwin ]; then
-      alias wine=/Applications/Wine.app/Contents/MacOS/Wine
-      alias adb=/Users/Kanta/Library/Android/sdk/platform-tools/adb
-      alias fastboot=/Users/Kanta/Library/Android/sdk/platform-tools/fastboot
-fi
-export PATH="/usr/local/sbin:/Library/TeX/texbin:~/Library/Android/sdk/ndk-bundle:/Users/Kanta/Library/Android/sdk/platform-tools:~/go/bin:GNUTERM=aqua /usr/local/bin/gnuplot:$PATH"
-export PATH=/usr/local/Cellar/openssl/1.0.2/bin:/opt/local/bin:/opt/local/sbin:/Library/TeX/texbin:~/Library/Android/sdk/ndk-bundle:/Users/Kanta/Library/Android/sdk/platform-tools:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin:/opt/X11/bin:/usr/local/squid/sbin/ 
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
-#ln -sf ~/dotfiles/.zshrc ~/.zshrc
-#ln -sf ~/dotfiles/.zprofile ~/.zprofile
+set -u
+DOT_DIRECTORY="${HOME}/dotfiles"
+DOT_CONFIG_DIRECTORY=".config"
 
-ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
-ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
-sh ~/dotfiles/lndir.sh ~/dotfiles/zprezto ~/.zprezto/runcoms
+echo "link home directory dotfiles"
+cd ${DOT_DIRECTORY}
+for f in .??*
+do
+    #無視したいファイルやディレクトリ
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".config" ] && continue
+    ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+done
+
+echo "link .config directory dotfiles"
+cd ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}
+for file in `\find . -maxdepth 8 -type f`; do
+#./の2文字を削除するためにfile:2としている
+    ln -snfv ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}/${file:2} ${HOME}/${DOT_CONFIG_DIRECTORY}/${file:2}
+done
+
+echo "linked dotfiles complete!"
